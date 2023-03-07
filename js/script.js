6049fa25e45a4abe9fc08046b3c0c6e59fa31bf7
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-03-07 15:51:12 +0100

    Add: style and keyframes to password generator

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Raccolta dati dell'utente
+//Chiedo il nome
+const nome = prompt("Come ti chiami?");
+console.log(name);
+
+//Chiedo il cognome
+const cognome = prompt("Qual è il tuo cognome?");
+console.log(cognome);
+
+//Chiedo il colore preferito
+const colore = prompt("Qual è il tuo colore preferito?");
+console.log(colore);
+
+//Preparo il risultato della password generata
+const pswGenerata = nome + cognome + colore + 21;
+console.log(pswGenerata)
+
+//Stampo la password generata nella pagina
+document.getElementById("password").innerHTML = pswGenerata;
